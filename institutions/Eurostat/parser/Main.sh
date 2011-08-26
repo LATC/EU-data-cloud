6d759d3003641105436df6350941488a7694df26
Aftab Iqbal <aftabiq@gmail.com>
2011-08-26 11:27:02 +0100

    script updated

--- institutions/Eurostat/parser/Main.sh
+++ institutions/Eurostat/parser/Main.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+startTime=$(date)
 FILES=~/downloadZip/zip/*
 unCompressPath=~/downloadZip/UnCompress/
 dsdPath=~/downloadZip/dsd/
 dataPath=~/downloadZip/data/
+i=1
 for f in $FILES
 do
-  echo "UnCompressing $f"
+  echo "UnCompressing file#$i ... filename is $f"
   sh UnCompressFile.sh -p $unCompressPath -u $f
+  i=`expr $i + 1`
 done
 
+i=1
 for f in $unCompressPath*
 do
-  echo "Processing $f file..."
+  echo "Processing file#$i ... filename is $f"
   echo $f | grep -qE ".dsd.xml"
   if [ $? -eq 0 ]
    then
@@ -23,6 +27,9 @@ do
       filename=${f##*/}
       fname=`echo $filename | awk '{ print substr($filename,0,length($filename)-8)}'`
       sh SDMXParser.sh -f $fname -o $dataPath
+      i=`expr $i + 1`
    fi
   fi
 done
+echo "Shell script started the job at $startTime"
+echo "Shell script finished the job at $(date)"
