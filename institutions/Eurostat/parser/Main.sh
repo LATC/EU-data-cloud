1fcabc2280edb63102c7ee76738342502771744a
Aftab Iqbal <aftabiq@gmail.com>
2011-08-26 12:08:26 +0100

    few comments added to the script

--- institutions/Eurostat/parser/Main.sh
+++ institutions/Eurostat/parser/Main.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 startTime=$(date)
+# directory path where all the zip files are stored
 FILES=~/downloadZip/zip/*
+
+#directory path where the uncompressed file should be stored
 unCompressPath=~/downloadZip/UnCompress/
+
+#directory path where dsd (RDFs) will be stored
 dsdPath=~/downloadZip/dsd/
+
+#directory path where sdmx (RDFs) will be stored
 dataPath=~/downloadZip/data/
+
 i=1
 for f in $FILES
 do
