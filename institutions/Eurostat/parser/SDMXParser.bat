c4b9860d24496a4fadf43df456bb4e64866d7afc
Aftab Iqbal <aftabiq@gmail.com>
2011-09-07 17:21:04 +0100

    memory allocation parameter reverts to 512m

--- institutions/Eurostat/parser/SDMXParser.bat
+++ institutions/Eurostat/parser/SDMXParser.bat
@@ -2,7 +2,7 @@
 set EuroStat=%~p0
 set CP="%EuroStat%build"
 call :findjars "%EuroStat%lib"
-java -cp %CP% -Xmx2G com.ontologycentral.estatwrap.SDMXParser %*
+java -cp %CP% -Xmx512M com.ontologycentral.estatwrap.SDMXParser %*
 
 :findjars
 for %%j in (%1\*.jar) do call :addjar "%%j"
