4614fb67845bef3be62221b311f0fef6c76dcad9
Aftab Iqbal <aftabiq@gmail.com>
2012-01-23 12:21:10 +0000

    parameters added to the script

--- institutions/Eurostat/parser/DiffToC.sh
+++ institutions/Eurostat/parser/DiffToC.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp "./build:./lib/*" -Xmx512M org.deri.eurostat.toc.DiffToC "$@"
+java -cp "./build:./lib/*" -Xmx512M org.deri.eurostat.toc.DiffToC -i "/downloadZip/ToC/table_of_contents.xml" -o "/downloadZip/ToC/" -f "/downloadZip/updateLogs/" -z "/downloadZip/tempZip/" -t "/downloadZip/tempData/" -s "/downloadZip/data/" -d "/downloadZip/dsd/" -l "/downloadZip/log/" -p "/downloadZip/original-data/" -r "/downloadZip/raw-data/"
