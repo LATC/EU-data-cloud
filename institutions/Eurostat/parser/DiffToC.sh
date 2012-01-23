058501927a11ffd14b98b3492f047061669b8318
Sarven Capadisli <sarven.capadisli@deri.org>
2012-01-23 13:38:40 +0000

    Paths reflect srvgal85

--- institutions/Eurostat/parser/DiffToC.sh
+++ institutions/Eurostat/parser/DiffToC.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp "./build:./lib/*" -Xmx512M org.deri.eurostat.toc.DiffToC -i "/downloadZip/ToC/table_of_contents.xml" -o "/downloadZip/ToC/" -f "/downloadZip/updateLogs/" -z "/downloadZip/tempZip/" -t "/downloadZip/tempData/" -s "/downloadZip/data/" -d "/downloadZip/dsd/" -l "/downloadZip/log/" -p "/downloadZip/original-data/" -r "/downloadZip/raw-data/"
+java -cp "./build:./lib/*" -Xmx512M org.deri.eurostat.toc.DiffToC -i "/data/eurostat/toc/table_of_contents.xml" -o "/data/eurostat/toc/" -f "/data/eurostat/update-logs/" -z "/data/eurostat/temp-zip/" -t "/data/eurostat/temp-data" -s "/data/eurostat/data/" -d "/data/eurostat/dsd/" -l "/data/eurostat/logs/" -p "/data/eurostat/original-data/" -r "/data/eurostat/raw-data/"
