2ae0e6121f99c58b8f3231e8d75b20917ddba57c
Knud Möller <dunken69@gmail.com>
2012-02-17 12:25:41 +0100

    added qb:Slice object, page object, labels, etc.

--- institutions/ecb-stats/sdmx2rdfa/sdmx2rdfa.php
+++ institutions/ecb-stats/sdmx2rdfa/sdmx2rdfa.php
@@ -1,5 +1,40 @@
 <?php
 
+$sdmx_source = $argv[1];
+
+$reader = new XMLReader();
+
+$reader->open($sdmx_source);
+
+$series_id="unknown_series";
+$series_title="Unknown Title";
+$observations = array();
+$counter = 0;
+
+while ($reader->read()) {
+	if ($reader->localName=="Group") {
+		$node = $reader->expand();
+		$series_title = $node->getAttribute('TITLE_COMPL');
+	} elseif ($reader->localName=="Series") {
+		$node = $reader->expand();
+		$series_id = $node->getAttribute('DOM_SER_IDS');
+	} elseif ($reader->localName=="Obs") {
+		$node = $reader->expand();
+		// <Obs TIME_PERIOD="1988" OBS_VALUE="2.75" OBS_STATUS="A" OBS_CONF="F" />
+		$observation = array();
+		$observation['id'] = $series_id . "_" . $counter;
+		$observation['period'] = $node->getAttribute('TIME_PERIOD');
+		$observation['value'] = $node->getAttribute('OBS_VALUE');
+		$observation['status'] = $node->getAttribute('OBS_STATUS');
+		$observation['conf'] = $node->getAttribute('OBS_CONF');
+		$observations[] = $observation;
+		$counter++;
+	}
+
+}
+
+$reader->close();
+
 $header = <<<HEADER
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
@@ -8,60 +43,45 @@ $header = <<<HEADER
 	xmlns:qb="http://purl.org/linked-data/cube#"
 	xmlns:sdmx-measure="http://purl.org/linked-data/sdmx/2009/measure#"
 	xmlns:sdmx-dim="http://purl.org/linked-data/sdmx/2009/dimension#"
-	xmlns:sioc="http://rdfs.org/sioc/ns#">
-<head><title>ECB Data Series</title></head>
+	xmlns:sioc="http://rdfs.org/sioc/ns#"
+	xmlns:foaf="http://xmlns.com/foaf/0.1/">
+<head><title>ECB Data Series | $series_title</title></head>
 <body typeof="sioc:Site" about="">
-<table>
+<h1 property="rdfs:label">$series_title</h1>
+<table about="#$series_id" typeof="qb:Slice" rel="foaf:primaryTopicOf" href="" property="rdfs:label" content="$series_title">
 	<tr>
 		<th>Period</th>
 		<th>Value</th>
 		<th>Status</th>
 		<th>Conf</th>
 	</tr>
+	<div about="#$series_id" rel="qb:observation">
 
 HEADER;
 
 echo $header;
 
-$sdmx_source = $argv[1];
-
-$reader = new XMLReader();
-
-$reader->open($sdmx_source);
-
-$series_id="unknown_series";
-$counter = 0;
-while ($reader->read()) {
-	if ($reader->localName=="Series") {
-		$node = $reader->expand();
-		$series_id = $node->getAttribute('DOM_SER_IDS');
-	} elseif ($reader->localName=="Obs") {
-		$node = $reader->expand();
-		// <Obs TIME_PERIOD="1988" OBS_VALUE="2.75" OBS_STATUS="A" OBS_CONF="F" />
-		$period = $node->getAttribute('TIME_PERIOD');
-		$time_uri = "http://reference.data.gov.uk/id/year/" . $period;
-		$value = $node->getAttribute('OBS_VALUE');
-		$status = $node->getAttribute('OBS_STATUS');
-		$conf = $node->getAttribute('OBS_CONF');
-		$row_id = $series_id . "_" . $counter;
-		$table_row = <<<ROW
-	<tr about="#$row_id" typeof="qb:Observation">
-		<td rel="sdmx-dim:refPeriod"><a href="$time_uri" property="rdfs:label">$period</a></td>
+foreach ($observations as $observation) {
+	$id = $observation['id'];
+	$period = $observation['period'];
+	$value = $observation['value'];
+	$status = $observation['status'];
+	$conf = $observation['conf'];
+	$table_row = <<<ROW
+	<tr about="#$id" typeof="qb:Observation">
+		<td rel="sdmx-dim:refPeriod"><a href="http://reference.data.gov.uk/id/year/$period" property="rdfs:label">$period</a></td>
 		<td property="sdmx-measure:obsValue">$value</td>
 		<td>$status</td>
 		<td>$conf</td>
 	</tr>
 
 ROW;
-		echo $table_row;
-		$counter++;
-	}
-
+	echo $table_row;
 }
 
-$reader->close();
 
 $footer = <<<FOOTER
+	</div>
 </table>
 </body>
 </html>
