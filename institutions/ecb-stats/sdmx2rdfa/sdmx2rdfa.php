8b7bdb770bb3f7c865693c8b50d886bbe2e239ea
Keith Alexander <github@keithalexander.co.uk>
2012-02-26 21:03:17 +0000

    corrected include path #8

--- institutions/ecb-stats/sdmx2rdfa/sdmx2rdfa.php
+++ institutions/ecb-stats/sdmx2rdfa/sdmx2rdfa.php
@@ -1,6 +1,6 @@
 <?php
 
-set_include_path(get_include_path().':'.basename(dirname(__FILE__)).'');
+set_include_path(get_include_path().':'.dirname(dirname(__FILE__)).'');
 require '../inc.php';
 
 $series_id=str_replace('_','.',$_GET['serieskey']);
