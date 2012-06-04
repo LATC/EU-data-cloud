02660b3698b1af183ef23f264f007a3390f65636
Aftab Iqbal <aftabiq@gmail.com>
2012-06-04 19:39:56 +0100

    Main script updated

--- institutions/Eurostat/parser/Main.sh
+++ institutions/Eurostat/parser/Main.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 startTime=$(date)
 #directory path where all the zip files are stored
-FILES=/data/eurostat/original-data/
+FILES=/data/sandbox/eurostat/original-data/
 
 #directory path where the uncompressed file should be stored
-unCompressPath=/data/eurostat/raw-data/
+unCompressPath=/data/sandbox/eurostat/raw-data/
+
+#directory path where the tsv file should be stored
+tsvPath=/data/sandbox/eurostat/tsv/
 
 #directory path where dsd (RDFs) will be stored
-dsdPath=/data/eurostat/dsd/
+dsdPath=/data/sandbox/eurostat/dsd/
 
 #directory path where sdmx (RDFs) will be stored
-dataPath=/data/eurostat/data/
+dataPath=/data/sandbox/eurostat/data/
 
 #directory path where log file will be
-logPath=/data/eurostat/logs/
+logPath=/data/sandbox/eurostat/logs/
 
+#directory path where sdmx-code file is located.
+sdmxFile=/data/sdmx-code.ttl
+
+ext=".tsv.gz"
 
 ### Deleting files from directories if exists
 
@@ -60,14 +67,14 @@ do
   echo $f | grep -qE ".dsd.xml"
   if [ $? -eq 0 ]
    then
-     sh DSDParser.sh -o $dsdPath -i $f -f turtle
+     sh DSDParser.sh -o $dsdPath -i $f -f turtle -a $sdmxFile
   else
    echo $f | grep -qE ".sdmx.xml"
    if [ $? -eq 0 ]
     then
       filename=${f##*/}
       fname=`echo $filename | awk '{ print substr($filename,0,length($filename)-8)}'`
-      sh SDMXParser.sh -f $fname -o $dataPath -i $f -l $logPath
+      sh SDMXParser.sh -f $fname -o $dataPath -i $f -l $logPath -t $tsvPath$fname$ext
       i=`expr $i + 1`
    fi
 fi
